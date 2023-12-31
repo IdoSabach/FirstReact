c3bdc97a2b934f77e601e67b06aad79a592ee6d8
Ido <Idosabach11@gmail.com>
2023-12-31 16:32:45 +0200

    first commit

---
+++ my-first-react-app/src/Greeting.jsx
@@ -0,0 +1,16 @@
+function Greeting() {
+  const image = "./Screenshot 2023-12-30 213133.png"
+  return (
+    <>
+      <h1>&quot;I swear by my pretty floral bonnet, I will end you.&quot;</h1>
+      <p>idododod</p>
+      <img src={image} alt="" />
+      <div className="ido">i love eat pizza and milk</div>
+      <div className="ido">i love eat pizza and milk</div>
+      <div className="ido">i love eat pizza and milk</div>
+      <div className="ido">i love eat pizza and milk</div>
+    </>
+  );
+}
+
+export default Greeting;
