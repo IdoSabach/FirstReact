c61f86e8a558a23003a281fec6027690d95dce07
Ido <Idosabach11@gmail.com>
2024-01-20 19:26:17 +0200

    test tic tac toe

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,76 @@
 import Button from "./components/Button";
-import {useState} from 'react'
+import { useState } from "react";
 
 export default function App() {
-  const [curr , setCurr] = useState()
+  const [curr, setCurr] = useState();
   const arr = {
-    text1: "ido1",
-    text2: "ido2",
-    text3: "ido3",
-    text4: "ido4",
-    text5: "ido5",
-    text6: "ido6",
-  }
+    text1: "1",
+    text2: "2",
+    text3: "3",
+    text4: "4",
+    text5: "5",
+    text6: "6",
+    text7: "7",
+    text8: "8",
+    text9: "9",
+  };
 
   function handleClick(name) {
-    setCurr(name)
+    setCurr(name);
     console.log(name);
   }
 
   return (
     <>
-      <section className="sectionOne">
-        <Button text={"Click me"} onSelect={() => handleClick(arr.text1)} curr={curr === "ido1"}/>
-        <Button text={"Click me"} onSelect={() => handleClick(arr.text2)} curr={curr === "ido2"}/>
-        <Button text={"Click me"} onSelect={() => handleClick(arr.text3)} curr={curr === "ido3"}/>
-        <Button text={"Click me"} onSelect={() => handleClick(arr.text4)} curr={curr === "ido4"}/>
-        <Button text={"Click me"} onSelect={() => handleClick(arr.text5)} curr={curr === "ido5"}/>
-        <Button text={"Click me"} onSelect={() => handleClick(arr.text6)} curr={curr === "ido6"}/>
+      <header className="head">Title of page</header>
+      <section className="mainBox">
+        <section className="sectionOne">
+          <Button
+            text={"Click me"}
+            onSelect={() => handleClick(arr.text1)}
+            curr={curr === "1"}
+          />
+          <Button
+            text={"Click me"}
+            onSelect={() => handleClick(arr.text2)}
+            curr={curr === "2"}
+          />
+          <Button
+            text={"Click me"}
+            onSelect={() => handleClick(arr.text3)}
+            curr={curr === "3"}
+          />
+          <Button
+            text={"Click me"}
+            onSelect={() => handleClick(arr.text4)}
+            curr={curr === "4"}
+          />
+          <Button
+            text={"Click me"}
+            onSelect={() => handleClick(arr.text5)}
+            curr={curr === "5"}
+          />
+          <Button
+            text={"Click me"}
+            onSelect={() => handleClick(arr.text6)}
+            curr={curr === "6"}
+          />
+          <Button
+            text={"Click me"}
+            onSelect={() => handleClick(arr.text7)}
+            curr={curr === "6"}
+          />
+          <Button
+            text={"Click me"}
+            onSelect={() => handleClick(arr.text8)}
+            curr={curr === "6"}
+          />
+          <Button
+            text={"Click me"}
+            onSelect={() => handleClick(arr.text9)}
+            curr={curr === "6"}
+          />
+        </section>
       </section>
     </>
   );
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,12 @@
-export default function Button({text , onSelect , curr}){
+export default function Button({ text, onSelect, curr }) {
   return (
     <>
-      <button className={curr ? 'active' : undefined} onClick={onSelect}>{text}</button>
+      <button
+        className="btn"
+        onClick={onSelect}
+      >
+        {text}
+      </button>
     </>
-  )
+  );
 }
